2d45cd959229c81817278ec6aa2fac8b95b858cc
aran26ef <khanaleran3@gmail.com>
2026-09-09 13:11:11 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+function showMessage(){
+  alert('Thank you for contacting RK Pashmina Store! We will add your real WhatsApp number and contact details soon.');
+}
